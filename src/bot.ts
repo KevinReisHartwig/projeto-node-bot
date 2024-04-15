9c2152da08931a2a37574738db4bf5fab5e11219
Alunos Laboratorio <aluno.laboratorio@faesa.br>
2024-04-15 20:20:20 -0300

    prisma

--- src/bot.ts
+++ src/bot.ts
@@ -1,12 +1,13 @@
 import TelegramBot from "node-telegram-bot-api";
+import { PrismaClient } from '@prisma/client';
 import { config } from "dotenv";
 
 config();
 
+const prisma = new PrismaClient();
 const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
 
 bot.onText(/\/echo (.+)/, (msg, match) => {
-
   const chatId = msg.chat.id;
   if(match){
     const resp = match[1];
@@ -14,25 +15,38 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
   }
 });
 
-bot.on('photo', async (msg) => {
+bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
-  if (!msg.photo || msg.photo.length === 0) {
-    console.error("Nenhuma foto encontrada na mensagem.");
-    return;
-  }
-  
-  const photoId = msg.photo[msg.photo.length - 1].file_id;
-  
-  try {
-    const photo = await bot.downloadFile(photoId, "./src/imgs");
-    bot.sendPhoto(chatId, photo, { caption: "Foto enviada" });
-  } catch (error) {
-    console.error("Erro ao baixar ou enviar a foto:", error);
+  const currentTime = new Date().getHours();
+
+  if (currentTime >= 9 && currentTime < 18) {
+    bot.sendMessage(chatId, 'Olá! Aqui está o link: https://faesa.br');
+  } else {
+    bot.sendMessage(chatId, 'Desculpe, estamos fora do horário comercial, O horário comercial é das 09:00 às 18:00. Por favor, forneça seu e-mail para que possamos entrar em contato.');
   }
 });
 
-bot.on('message', (msg) => {
+bot.on('text', async (msg) => {
   const chatId = msg.chat.id;
+  const currentTime = new Date().getHours();
+  const email = msg.text;
 
-  bot.sendMessage(chatId, 'msg recebida com sucesso');
+  if (currentTime < 9 || currentTime >= 18) {
+    if (typeof email === 'string') {
+      try {
+        await prisma.email.create({
+          data: {
+            email: email
+          }
+        });
+        bot.sendMessage(chatId, 'Obrigado! Seu e-mail foi registrado para contato futuro.');
+      } catch (error) {
+        console.error("Erro ao armazenar o e-mail:", error);
+        bot.sendMessage(chatId, 'Desculpe, ocorreu um erro ao processar sua solicitação. Por favor, tente novamente mais tarde.');
+      }
+    } else {
+      console.error("E-mail recebido é undefined ou não é uma string.");
+      bot.sendMessage(chatId, 'Desculpe, ocorreu um erro ao processar sua solicitação. Por favor, tente novamente mais tarde.');
+    }
+  }
 });
